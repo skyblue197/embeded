76cd2d1f02ce1cc0e8050ee237184a2c1be7c38d
mkm4263 <mkm4263@naver.com>
2016-12-05 16:22:49 +0900

    Add files via upload

---
+++ app/src/main/java/info/androidhive/firebase/app/AppConfig.java
@@ -0,0 +1,21 @@
+package info.androidhive.firebase.app;
+
+/**
+ * Created by jeon-yoseb on 2016. 11. 26..
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://219.254.60.31:80/android_login_api/login.php";
+    // Server user register url
+    public static String URL_REGISTER = "http://219.254.60.31:80/android_login_api/register.php";
+
+    public static String URL_SETCHAT = "http://219.254.60.31/chat/SetChat.php";
+
+    public static String URL_GETCHAT = "http://219.254.60.31/chat/GetChat.php";
+
+    public static String CHAT_EMAIL;
+
+    public AppConfig(){}
+
+}
---
+++ app/src/main/java/info/androidhive/firebase/app/AppController.java
@@ -0,0 +1,33 @@
+package info.androidhive.firebase.app;
+
+import android.app.Application;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class AppController extends Application {
+
+    public static final String TAG = AppController.class.getSimpleName();
+
+    private RequestQueue mRequestQueue;
+
+    private static AppController mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+}
